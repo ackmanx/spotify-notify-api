4749abb2d1d177462feddb017af25aa4c7a9cc99
ackmanx <ackmanx@users.noreply.github.com>
2021-12-11 11:03:03 -0600

    Using spotify library to authenticate. Maybe it worked

--- src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
+++ src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
@@ -1,5 +1,7 @@
 package com.ialreadysawthat.plugins
 
+import com.adamratzman.spotify.SpotifyScope
+import com.adamratzman.spotify.getSpotifyAuthorizationUrl
 import io.ktor.auth.*
 import io.ktor.util.*
 import io.ktor.client.*
@@ -14,53 +16,29 @@ import io.ktor.routing.*
 
 fun Application.configureSecurity() {
 
-    val client_id = "e466e7a286954e13b501d8e7bc4669cc" //this is the ID for my application registered with Spotify
-    val client_secret = "76f2fd5049574e09899dcf84810e5fef" //this will live in heroku config so it isn't exposed via git, but until then store it here and regen a new key later
-    val scope = "user-follow-read playlist-modify-public user-library-read playlist-read-private"
-    val stateKey = "spotify_auth_state"
-    val redirect_url = "http://me:8080/auth/callback"
+  val client_id = "e466e7a286954e13b501d8e7bc4669cc" //this is the ID for my application registered with Spotify
+  val client_secret = "76f2fd5049574e09899dcf84810e5fef" //this will live in heroku config so it isn't exposed via git, but until then store it here and regen a new key later
+  // val scope = "user-follow-read playlist-modify-public user-library-read playlist-read-private"
+  val stateKey = "spotify_auth_state"
+  val redirect_url = "http://me:8080/callback"
 
-    install(Authentication) {
-        oauth("auth-oauth-google") {
-            urlProvider = { "http://localhost:8080/callback" }
-            providerLookup = {
-                OAuthServerSettings.OAuth2ServerSettings(
-                    name = "google",
-                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-                    requestMethod = HttpMethod.Post,
-                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
-                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
-                )
-            }
-            client = HttpClient(Apache)
-        }
+  routing {
+    get("login") {
+      val url: String = getSpotifyAuthorizationUrl(
+        SpotifyScope.USER_FOLLOW_READ,
+        SpotifyScope.PLAYLIST_MODIFY_PUBLIC,
+        SpotifyScope.USER_LIBRARY_READ,
+        SpotifyScope.PLAYLIST_READ_PRIVATE,
+        clientId = client_id,
+        redirectUri = redirect_url,
+        state = stateKey
+      )
+
+      call.respondRedirect(url)
     }
 
-    data class MySession(val count: Int = 0)
-    install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
+    get("/callback") {
+      call.respondRedirect("/location/eric")
     }
-
-    routing {
-        authenticate("auth-oauth-google") {
-            get("login") {
-                call.respondRedirect("/callback")
-            }
-
-            get("/callback") {
-                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
-                call.sessions.set(UserSession(principal?.accessToken.toString()))
-                call.respondRedirect("/hello")
-            }
-        }
-        get("/session/increment") {
-            val session = call.sessions.get<MySession>() ?: MySession()
-            call.sessions.set(session.copy(count = session.count + 1))
-            call.respondText("Counter is ${session.count}. Refresh to increment.")
-        }
-    }
+  }
 }
