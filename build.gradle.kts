cb1426bc296a856e2b0a115c334399993e91e330
ackmanx <ackmanx@users.noreply.github.com>
2021-12-11 09:40:15 -0600

    Spotify stuff I might need

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.adamratzman:spotify-api-kotlin-core:3.8.4")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
--- src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
+++ src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
@@ -14,6 +14,12 @@ import io.ktor.routing.*
 
 fun Application.configureSecurity() {
 
+    val client_id = "e466e7a286954e13b501d8e7bc4669cc" //this is the ID for my application registered with Spotify
+    val client_secret = "76f2fd5049574e09899dcf84810e5fef" //this will live in heroku config so it isn't exposed via git, but until then store it here and regen a new key later
+    val scope = "user-follow-read playlist-modify-public user-library-read playlist-read-private"
+    val stateKey = "spotify_auth_state"
+    val redirect_url = "http://me:8080/auth/callback"
+
     install(Authentication) {
         oauth("auth-oauth-google") {
             urlProvider = { "http://localhost:8080/callback" }
@@ -31,6 +37,7 @@ fun Application.configureSecurity() {
             client = HttpClient(Apache)
         }
     }
+
     data class MySession(val count: Int = 0)
     install(Sessions) {
         cookie<MySession>("MY_SESSION") {
