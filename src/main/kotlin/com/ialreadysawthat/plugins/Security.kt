fd304dc2f6b45f422835af747fd48c81c4bbbf42
ackmanx <ackmanx@users.noreply.github.com>
2021-12-11 18:59:27 -0600

    Can get api from library. Connection to spotify works but not sure why because I'm in the callback handler and provided a redirect to callback. Maybe they dont transfer you there the 2nd time but just match what had before

--- src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
+++ src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
@@ -1,17 +1,11 @@
 package com.ialreadysawthat.plugins
 
 import com.adamratzman.spotify.SpotifyScope
+import com.adamratzman.spotify.SpotifyUserAuthorization
 import com.adamratzman.spotify.getSpotifyAuthorizationUrl
-import io.ktor.auth.*
-import io.ktor.util.*
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.locations.*
-import io.ktor.http.*
-import io.ktor.sessions.*
+import com.adamratzman.spotify.spotifyClientApi
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 
 fun Application.configureSecurity() {
@@ -19,7 +13,6 @@ fun Application.configureSecurity() {
   val client_id = "e466e7a286954e13b501d8e7bc4669cc" //this is the ID for my application registered with Spotify
   val client_secret = "76f2fd5049574e09899dcf84810e5fef" //this will live in heroku config so it isn't exposed via git, but until then store it here and regen a new key later
   // val scope = "user-follow-read playlist-modify-public user-library-read playlist-read-private"
-  val stateKey = "spotify_auth_state"
   val redirect_url = "http://me:8080/callback"
 
   routing {
@@ -31,14 +24,18 @@ fun Application.configureSecurity() {
         SpotifyScope.PLAYLIST_READ_PRIVATE,
         clientId = client_id,
         redirectUri = redirect_url,
-        state = stateKey
       )
 
       call.respondRedirect(url)
     }
 
     get("/callback") {
-      call.respondRedirect("/location/eric")
+      val code = call.request.queryParameters["code"] ?: ""
+      val authorization = SpotifyUserAuthorization(authorizationCode = code)
+
+      val api = spotifyClientApi(client_id, client_secret, "http://me:8080/callback", authorization).build()
+
+      call.respond("It worked!")
     }
   }
 }
