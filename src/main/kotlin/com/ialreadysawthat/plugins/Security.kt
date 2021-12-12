e5dcd2518066975e5daea34e80d44f0a743056dc
ackmanx <ackmanx@users.noreply.github.com>
2021-12-11 19:51:22 -0600

    Redirect to heroku if not local

--- src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
+++ src/main/kotlin/com/ialreadysawthat/plugins/Security.kt
@@ -13,8 +13,9 @@ fun Application.configureSecurity() {
 
   val client_id = "e466e7a286954e13b501d8e7bc4669cc" //this is the ID for my application registered with Spotify
   val client_secret = "76f2fd5049574e09899dcf84810e5fef" //this will live in heroku config so it isn't exposed via git, but until then store it here and regen a new key later
-  // val scope = "user-follow-read playlist-modify-public user-library-read playlist-read-private"
-  val redirect_url = "http://me:8080/callback"
+  val redirect_url =
+    if (System.getenv("ENV") == "local") "http://me:8080/callback"
+    else "http://spotify-notify-api.herokuapp.com/callback"
 
   routing {
     get("login") {
